be9ca304889c3c6130c50ac90fea711aaeeb8a5d
Aneeshkumar M <79455981+Aneeshkumar2001@users.noreply.github.com>
2022-11-16 07:38:13 +0530

    Add files via upload

---
+++ Assignments/AneeshKumar/Assignment 1/app.py
@@ -0,0 +1,23 @@
+from urllib import request
+from flask import Flask, redirect, url_for, render_template,request
+
+app = Flask(__name__)
+
+#Route
+@app.route('/')
+def home():
+    return "Hi"
+
+@app.route('/register',methods = ['POST', 'GET'])
+def register():
+    if request.method=='POST':
+        return redirect(url_for('display',user = request.form['email']))
+    return render_template("register.html")
+
+@app.route('/display/<user>',methods=['POST','GET'])
+def display(user):
+    return "registered mail is "+user
+
+#Main Function
+if __name__=='__main__':
+    app.run()
